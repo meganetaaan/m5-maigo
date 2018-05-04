e94b63d5c700dc9998c7539ca2aaf020ae311654
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2018-05-04 20:24:24 +0900

    It works

--- src/main.ino
+++ src/main.ino
@@ -1,6 +1,7 @@
 #include <M5Stack.h>
 #include "Free_Fonts.h"
 #include <Wire.h>
+#include <AquesTalkTTS.h>
 #include <avator.h>
 #include <BLEDevice.h>
 #include <BLEServer.h>
@@ -9,7 +10,85 @@
 
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
+bool lost = false;
 double t0, t1;
+
+Avator *avator;
+int count = 0;
+float f = 0;
+float last = 0;
+const Expression expressions[] = {
+  Angry,
+  Sleepy,
+  Happy,
+  Sad
+};
+
+void talkLoop(void *args)
+{
+  for(;;)
+  {
+    if (lost && TTS.getLevel() == 0)
+    {
+      if (rand() > 0.5)
+      {
+        TTS.play("ta'roukunn.to'-channo/saga_shite", 100);
+      }
+      else
+      {
+        TTS.play("to'-chanha/doko?", 100);
+      }
+    }
+    delay(8000);
+  }
+}
+void breath(void *args)
+{
+  int c = 0;
+  for(;;)
+  {
+    c = c + 1 % 100;
+    float f = sin(c * 2 * PI / 100.0);
+    avator->setBreath(f);
+    delay(33);
+  }
+}
+void drawLoop(void *args)
+{
+  for(;;)
+  {
+    int level = TTS.getLevel();
+    float f = level / 12000.0;
+    float open = min(1.0, last + f / 2.0);
+    last = f;
+    avator->setMouthOpen(open);
+    avator->draw();
+    delay(33);
+  }
+}
+
+void saccade(void *args)
+{
+  for(;;)
+  {
+    float vertical = (float)rand()/(float)(RAND_MAX / 2) - 1;
+    float horizontal = (float)rand()/(float)(RAND_MAX / 2) - 1;
+    avator->setGaze(vertical, horizontal);
+    delay(500 + 100 * random(20));
+  }
+}
+
+void blink(void *args)
+{
+  for(;;)
+  {
+    avator->setEyeOpen(1);
+    delay(2500 + 100 * random(20));
+    avator->setEyeOpen(0);
+    delay(300 + 10 * random(20));
+  }
+}
+
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
@@ -20,15 +99,23 @@ double t0, t1;
 
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
-      M5.Lcd.clear();
-      M5.Lcd.drawString("connected", 40, 40);
+      M5.Lcd.fillRect(0, 0, 240, 30, TFT_BLACK);
+      M5.Lcd.drawString("connected", 10, 220);
+      avator->setExpression(Happy);
+      TTS.play("ta'roukunn.to'-channno/_chika'kude/asobo'-.", 80);
+      delay(3000);
+      avator->setExpression(Neutral);
       deviceConnected = true;
+      lost = false;
     };
 
     void onDisconnect(BLEServer* pServer) {
-      M5.Lcd.clear();
-      M5.Lcd.drawString("disconnected", 40, 40);
+      M5.Lcd.fillRect(0, 0, 240, 30, TFT_BLACK);
+      M5.Lcd.drawString("disconnected", 10, 220);
+      avator->setExpression(Sad);
+      TTS.play("do'kka/icchatta'yo-.", 100);
       deviceConnected = false;
+      lost = true;
     }
 };
 
@@ -52,16 +139,59 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 };
 
 void setup() {
+  int iret;
+  iret = TTS.create(NULL);
   Serial.begin(115200);
   delay(1000);
+  avator = new Avator();
+  xTaskCreatePinnedToCore(
+                    talkLoop,     /* Function to implement the task */
+                    "talkLoop",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    1,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    0);        /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+                    drawLoop,     /* Function to implement the task */
+                    "drawLoop",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    1,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    1);        /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+                    saccade,     /* Function to implement the task */
+                    "saccade",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    3,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    1);        /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+                    breath,     /* Function to implement the task */
+                    "breath",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    2,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    1);        /* Core where the task should run */
+  xTaskCreatePinnedToCore(
+                    blink,     /* Function to implement the task */
+                    "blink",   /* Name of the task */
+                    4096,      /* Stack size in words */
+                    NULL,      /* Task input parameter */
+                    2,         /* Priority of the task */
+                    NULL,      /* Task handle. */
+                    1);        /* Core where the task should run */
   M5.begin();
-  M5.Lcd.setTextColor(TFT_BLACK, TFT_WHITE);
-  M5.Lcd.fillScreen(TFT_WHITE);
+  M5.Lcd.setBrightness(30);
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
   M5.Lcd.setFreeFont(FSSB12);
   // Select the font: FreeMono18pt7b – see Free_Fonts.h
   M5.Lcd.setFreeFont(FSS9); // FreeSans9pt7b
   // Create the BLE Device
-  BLEDevice::init("Test");
+  BLEDevice::init("Mai5");
   // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
